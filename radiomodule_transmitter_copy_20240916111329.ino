0a7a8f23738ef9665c06949818d511cbecfe32f6
ryanzhao <ryanzhao36@outlook.com>
2024-09-16 12:25:06 +0800

    Add files via upload

---
+++ radiomodule_transmitter_copy_20240916111329.ino
@@ -0,0 +1,29 @@
+
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
+
+
+// instantiate an object for the nRF24L01 transceiver
+RF24 radio(5, 6);
+
+
+void setup() {
+
+  Serial.begin(9600);
+  while (!Serial) {
+    // some boards need to wait to ensure access to serial over USB
+  }
+
+  // initialize the transceiver on the SPI bus
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}  // hold in infinite loop
+  }
+  else
+  {
+    Serial.println("You are ready to go!!!");
+  }
+
+  
+}  // loop
